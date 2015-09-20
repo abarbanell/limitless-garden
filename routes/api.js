c7d91481e0988afe8a40cfe63857488eb0bdee78
Tobias Abarbanell <tobias@abarbanell.de>
2015-09-20 21:48:23 +0200

    separated auth and rep fucntions for 3scale - slower, but could later be used to reduce calls.

--- routes/api.js
+++ routes/api.js
@@ -5,16 +5,41 @@ var router = express.Router();
 var ThreeScale = require('3scale').Client;
 client = new ThreeScale(process.env.THREESCALE_PROVIDER_KEY);
 var threescale = function(req, res, next) {
-  client.authrep_with_user_key({
-			"user_key": req.query.user_key,
-      "usage": { "hits": 1 } 
-  }, function(response){
-    if (response.is_success()) {
-			next();
-		} else {
-			res.status(403).send('not authorized: ' + response.error_message);
-		}
-  }); 
+
+console.log('3scale middleware start');
+	if (!req.query.user_key) {
+		res.status(400).send('Bad Request - user_key missing');
+	} else {
+		client.authorize_with_user_key({
+				"user_key": req.query.user_key
+		}, function(response){
+	console.log('3scale auth done');
+			if (response.is_success()) {
+
+	console.log('3scale auth success');
+				var trans = [{ "user_key": req.query.user_key, 
+					"usage": { "hits": 1 } 
+				}];
+				client.report(trans, function(response){
+	console.log('3scale rep done');
+					if (response.is_success()) {
+
+	console.log('3scale rep success');
+						next();
+					} else {
+
+	console.log('3scale auth fail');
+						res.status(401).send('no quota: ' + response.error_message);
+					};
+				});
+			} else {
+
+	console.log('3scale rep fail');
+				// error in authorization,
+					res.status(403).send('not authorized: ' + response.error_message);
+			}
+		}); 
+	};
 };
 router.use(threescale);
 
