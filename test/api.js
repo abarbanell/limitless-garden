497434cb016d9151695dd2bb20be940b47314999
Tobias Abarbanell <tobias@abarbanell.de>
2015-09-22 07:32:54 +0200

    added simple middleware test

--- test/api.js
+++ test/api.js
@@ -3,19 +3,18 @@
 var expect = require('expect.js');
 
 process.env.THREESCALE_PROVIDER_KEY='dummykey';
-var apiRouter = require('../routes/api.js');
+var apiRouter = require('../routes/api');
+var ts = require('../util/threescale');
 
 describe('API router tests', function() {
-  it('router should be valid', function(){
+  it('router should be valid', function(done){
       expect(apiRouter).to.be.ok();
 			expect(apiRouter.get).to.be.an('function');
+			done();
   });
 
-  it('dummy - check some syntax of expect.js', function(done) {
-    var f = function(){};
-    expect(f).to.be.an('function');
-    expect(f.doesnotexist).to.not.be.an('function'); // should fail
-    expect(f.bind).to.be.an('function'); // Function.prototype.bind()
+  it('threescale middleware tests', function(done) {
+    expect(ts).to.be.an('function');
 		done();
   })
 
