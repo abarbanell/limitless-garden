3b40854675d57c8bc588bc9ccacd31133a7ebc94
Tobias Abarbanell <tobias@abarbanell.de>
2015-09-25 21:27:26 +0200

    added route parameter and error route for api.

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var mongoskin = require('mongoskin');
+
+var mongourl = process.env.MONGOLAB_URI || 'mongodb://localhost:27107/test';
+var db = mongoskin.db(mongourl, {safe: true});
 
 var threescale = require('../util/threescale');
 router.use(threescale);
@@ -7,6 +11,15 @@ router.use(threescale);
 /* GET api  */
 router.get('/hello', function(req, res, next) {
   res.json({msg: 'hello world!'});
+});
+
+router.param('collectionName', function(req, res, next, collectionName){
+  req.collection = db.collection(collectionName)
+  return next()
+});
+
+router.get('/', function(req, res, next) {
+  res.json({ msg: 'please select a collection, e.g., /collections/messages'});
 });
 
 module.exports = router;
