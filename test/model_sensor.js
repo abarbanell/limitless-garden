1e05f4e467b691a65fa3f2105b3c81085ef62f00
Tobias Abarbanell <tobias@abarbanell.de>
2015-10-08 13:37:50 +0200

    added beforeEach and after to handle test data

--- test/model_sensor.js
+++ test/model_sensor.js
@@ -2,13 +2,34 @@ var expect = require('expect.js');
 var sensor = require('../model/sensor.js');
 var logger = require('../util/logger');
 var db = require('../util/db');
-
+var env = process.env.ENVIRONMENT || 'dev';
+var colname = env + '.sensor';
+var insertedId = "";
 
 describe('Sensor Model ', function() {
-	beforeEach(function() {
+	beforeEach(function(done) {
 		logger.info('beforeEach: remove and insert some data');
+		db.collection(colname).insert({}, function(err, result) {
+			logger.info('empty row inserted in collection '+ colname);
+			if (err) {
+				logger.error('beforeEach Error: ' + err);
+				done();
+			} 
+			logger.info('beforeEach result: ' + JSON.stringify(result));
+			insertedId = result.insertedIds[0];
+			done();
+		});
 	});
-
+	
+	after(function(done){
+		db.collection(colname).remove({}, function(err, result) {
+			if (err) {
+				logger.error('after() error: '+ err);
+			}
+			logger.info('after() - removed data: ' + JSON.stringify(result));
+			done();
+		});
+	});
   it('should contain get and getMulti methods ', function(){
       expect(sensor).to.be.an('object');
       expect(sensor.get).to.be.an('function');
