06127f9767ac3dcfcd576a8dc4baddebbc72d3df
Tobias Abarbanell <tobias@abarbanell.de>
2016-12-15 10:34:51 +0100

    spa: navbar layout (without router function)

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -21,6 +21,10 @@ module.exports = {
         loaders: ['awesome-typescript-loader', 'angular2-template-loader']
       },
       {
+      test: /\.html$/,
+      loader: 'html'
+      },
+      {
         test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
         loader: 'file?name=assets/[name].[hash].[ext]'
       }
--- src/app.component.ts
+++ src/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'spa-app',
   template: `
+    <navbar></navbar>
     <div class="container">
       <h1>Hello {{name}} !</h1>
     </div>
     `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { name = 'Limitless Spa'; }
