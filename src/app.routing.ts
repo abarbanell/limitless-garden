3e6757435e772a9a3ea5d81c361a6e92695d881c
Tobias Abarbanell <tobias@abarbanell.de>
2016-12-16 08:54:01 +0100

    routing

--- src/app.routing.ts
+++ src/app.routing.ts
@@ -4,5 +4,7 @@ import { Router, RouterModule} from '@angular/router';
 import { NotFoundComponent } from './not-found.component';
 
 export const routing = RouterModule.forRoot([
+    { path: '', component: NotFoundComponent }, 
+    { path: 'contact', component: NotFoundComponent }, 
     { path: '**', component: NotFoundComponent } 
 ]);
