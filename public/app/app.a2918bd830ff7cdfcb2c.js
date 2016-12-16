36e99763b713a3124c14951fc0c6853eb384f94e
Tobias Abarbanell <tobias@abarbanell.de>
2016-12-16 11:32:49 +0100

    included updated webpack artifacts

---
+++ public/app/app.a2918bd830ff7cdfcb2c.js
@@ -0,0 +1,168 @@
+webpackJsonp([0],{
+
+/***/ 0:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var platform_browser_dynamic_1 = __webpack_require__(1);
+	var app_module_1 = __webpack_require__(23);
+	platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
+
+
+/***/ },
+
+/***/ 23:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+	    return c > 3 && r && Object.defineProperty(target, key, r), r;
+	};
+	var __metadata = (this && this.__metadata) || function (k, v) {
+	    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+	};
+	var core_1 = __webpack_require__(3);
+	var platform_browser_1 = __webpack_require__(21);
+	var app_component_1 = __webpack_require__(24);
+	var navbar_component_1 = __webpack_require__(25);
+	var not_found_component_1 = __webpack_require__(27);
+	var app_routing_1 = __webpack_require__(28);
+	var AppModule = (function () {
+	    function AppModule() {
+	    }
+	    return AppModule;
+	}());
+	AppModule = __decorate([
+	    core_1.NgModule({
+	        imports: [platform_browser_1.BrowserModule, app_routing_1.routing],
+	        declarations: [app_component_1.AppComponent, navbar_component_1.NavbarComponent, not_found_component_1.NotFoundComponent],
+	        bootstrap: [app_component_1.AppComponent]
+	    }),
+	    __metadata("design:paramtypes", [])
+	], AppModule);
+	exports.AppModule = AppModule;
+
+
+/***/ },
+
+/***/ 24:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+	    return c > 3 && r && Object.defineProperty(target, key, r), r;
+	};
+	var __metadata = (this && this.__metadata) || function (k, v) {
+	    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+	};
+	var core_1 = __webpack_require__(3);
+	var AppComponent = (function () {
+	    function AppComponent() {
+	        this.name = 'Limitless Spa';
+	    }
+	    return AppComponent;
+	}());
+	AppComponent = __decorate([
+	    core_1.Component({
+	        selector: 'spa-app',
+	        template: "\n    <navbar></navbar>\n    <div class=\"container\">\n      <h1>Hello {{name}} !</h1>\n      <router-outlet></router-outlet>\n    </div>\n\n    ",
+	    }),
+	    __metadata("design:paramtypes", [])
+	], AppComponent);
+	exports.AppComponent = AppComponent;
+
+
+/***/ },
+
+/***/ 25:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+	    return c > 3 && r && Object.defineProperty(target, key, r), r;
+	};
+	var __metadata = (this && this.__metadata) || function (k, v) {
+	    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+	};
+	var core_1 = __webpack_require__(3);
+	var NavbarComponent = (function () {
+	    function NavbarComponent() {
+	    }
+	    return NavbarComponent;
+	}());
+	NavbarComponent = __decorate([
+	    core_1.Component({
+	        selector: 'navbar',
+	        template: __webpack_require__(26)
+	    }),
+	    __metadata("design:paramtypes", [])
+	], NavbarComponent);
+	exports.NavbarComponent = NavbarComponent;
+
+
+/***/ },
+
+/***/ 26:
+/***/ function(module, exports) {
+
+	module.exports = "<nav class=\"navbar navbar-inverse navbar-fixed-top\" role=\"navigation\">\n    <div class=\"container\">\n        <!-- Brand and toggle get grouped for better mobile display -->\n        <div class=\"navbar-header\">\n            <button type=\"button\" \n                class=\"navbar-toggle collapsed\" \n                data-toggle=\"collapse\" \n                data-target=\"#lgnavbar\"\n                aria-expanded=\"false\">\n                <span class=\"sr-only\">Toggle Navigation</span>\n                <span class=\"icon-bar\"></span>\n                <span class=\"icon-bar\"></span>\n                <span class=\"icon-bar\"></span>\n            </button>\n            <a class=\"navbar-brand\" href=\"#\">\n                <img src=\"/img/logo.png\" alt=\"Limitless Garden\" height=\"50\" width=\"50\">\n            </a>\n        </div>\n\n        <!-- Collect the nav links, forms, and other content for toggling -->\n        <div class=\"collapse navbar-collapse navbar-left\" id=\"lgnavbar\">\n            <ul class=\"nav navbar-nav\">\n                <li><a routerLink=\"app/\"> Spa Home </a></li>\n                <li><a routerLink=\"app/contact\">Contact</a></li>\n            </ul>\n        </div>\n        <!-- /.navbar-collapse -->\n    </div>\n    <!-- /.container-fluid -->\n</nav>";
+
+/***/ },
+
+/***/ 27:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+	    return c > 3 && r && Object.defineProperty(target, key, r), r;
+	};
+	var __metadata = (this && this.__metadata) || function (k, v) {
+	    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+	};
+	var core_1 = __webpack_require__(3);
+	var NotFoundComponent = (function () {
+	    function NotFoundComponent() {
+	    }
+	    return NotFoundComponent;
+	}());
+	NotFoundComponent = __decorate([
+	    core_1.Component({
+	        template: "\n        <div class=\"alert alert-danger\">\n            Component not found\n        </div>\n    "
+	    }),
+	    __metadata("design:paramtypes", [])
+	], NotFoundComponent);
+	exports.NotFoundComponent = NotFoundComponent;
+
+
+/***/ },
+
+/***/ 28:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var router_1 = __webpack_require__(29);
+	// import my components....
+	var not_found_component_1 = __webpack_require__(27);
+	exports.routing = router_1.RouterModule.forRoot([
+	    { path: '', component: not_found_component_1.NotFoundComponent },
+	    { path: 'contact', component: not_found_component_1.NotFoundComponent },
+	    { path: '**', component: not_found_component_1.NotFoundComponent }
+	]);
+
+
+/***/ }
+
+});
+//# sourceMappingURL=app.a2918bd830ff7cdfcb2c.js.map
