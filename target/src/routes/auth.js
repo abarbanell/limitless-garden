de2cf1d619c932e8c9057651178dae0b922d2593
Tobias Abarbanell <tobias@abarbanell.de>
2017-12-19 20:56:32 +0800

    bypassed Google Auth in local mode, but no session stub yet

--- target/src/routes/auth.js
+++ target/src/routes/auth.js
@@ -1,31 +1,41 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var express = require('express');
 var router = express.Router();
 var user = require('../model/user');
 var logger = require('../util/logger');
 var passport = require('passport');
+var authSetup_1 = require("../util/authSetup");
 // GET /auth/google
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  The first step in Google authentication will involve
 //   redirecting the user to google.com.  After authorization, Google
 //   will redirect the user back to this application at /auth/google/callback
-router.get('/google', passport.authenticate('google', { scope: [
-        'https://www.googleapis.com/auth/plus.login',
-        'https://www.googleapis.com/auth/plus.me'
-    ] }), 
-/* istanbul ignore next */ function (req, res) {
-    logger.err('/auth/google - should not reach this line');
-    // The request will be redirected to Google for authentication, so this
-    // function will not be called.
-});
-// GET /auth/google/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), 
-/* istanbul ignore next */ function (req, res) {
-    logger.info('google auth callback successful, now redirecting to home page');
-    res.redirect('/');
-});
+if (authSetup_1.AuthSetup.isLocal()) {
+    router.get('/google', function (req, res) {
+        res.redirect('/');
+    });
+}
+else {
+    router.get('/google', passport.authenticate('google', { scope: [
+            'https://www.googleapis.com/auth/plus.login',
+            'https://www.googleapis.com/auth/plus.me'
+        ] }), 
+    /* istanbul ignore next */ function (req, res) {
+        logger.err('/auth/google - should not reach this line');
+        // The request will be redirected to Google for authentication, so this
+        // function will not be called.
+    });
+    // GET /auth/google/callback
+    //   Use passport.authenticate() as route middleware to authenticate the
+    //   request.  If authentication fails, the user will be redirected back to the
+    //   login page.  Otherwise, the primary route function function will be called,
+    //   which, in this example, will redirect the user to the home page.
+    router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), 
+    /* istanbul ignore next */ function (req, res) {
+        logger.info('google auth callback successful, now redirecting to home page');
+        res.redirect('/');
+    });
+}
 module.exports = router;
 //# sourceMappingURL=auth.js.map
