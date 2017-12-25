f65bf372ef9cbd79ba4243598a0f53598578ccd2
Tobias Abarbanell <tobias@abarbanell.de>
2017-12-25 21:12:44 +0800

    dataService tests

--- public/spa/app/data.service.spec.ts
+++ public/spa/app/data.service.spec.ts
@@ -3,11 +3,12 @@ import { HttpClientTestingModule,
   HttpTestingController } from '@angular/common/http/testing';
 
 import { DataService } from './data.service';
+import { HttpHeaders } from '@angular/common/http';
 
 describe('DataService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-            imports: [
+      imports: [
         HttpClientTestingModule
       ],
       providers: [
@@ -22,8 +23,57 @@ describe('DataService', () => {
 
   it("should return collection list", async(() => {
     let service = TestBed.get(DataService);
+    let httpMock: HttpTestingController = TestBed.get(HttpTestingController)
+ 
     service.getCollections().subscribe(data => {
+      // reched only after the request is flushed (below)
       expect(data.length).toBe(3);      
+    },
+    err => {
+      expect("this").toBe("should not be reached");
     })
+
+    const req = httpMock.expectOne('/api/collections');
+    expect(req.request.method).toEqual('GET');
+    req.flush(["table1", "table2", "table3"])
+
+    httpMock.verify();
   }));
+
+  it("should return collection data", async(() => {
+    let service = TestBed.get(DataService);
+    let httpMock: HttpTestingController = TestBed.get(HttpTestingController)
+ 
+    service.getCollectionData("test").subscribe(data => {
+      // reched only after the request is flushed (below)
+      // expect(data).toBe(JSON.stringify(data));
+      expect(data.rows.length).toBe(3);
+      expect(data.count).toBe(17)      
+    },
+    err => {
+      expect("this").toBe("should not be reached");
+    })
+
+    const req = httpMock.expectOne('/api/collections/test');
+    expect(req.request.method).toEqual('GET');
+    
+    req.flush([ {
+      _id: "5a339c27328a1b2cb3442f6e",
+      date: "2017-10-17",
+      json: '{"_id":"5a339c27328a1b2cb3442f6e","field":"content"}'
+    }, {
+      _id: "5a339c27328a1b2cb3442f6f",
+      date: "2017-10-17",
+      json: '{"_id":"5a339c27328a1b2cb3442f6f","field":"content"}'
+    }, {
+      _id: "5a339c27328a1b2cb3442f6g",
+      date: "2017-10-17",
+      json: '{"_id":"5a339c27328a1b2cb3442f6g","field":"content"}'
+    }], {
+      headers: new HttpHeaders({"X-Total-Count":  "17"})
+    });
+
+    httpMock.verify();
+  }));
+
 });
